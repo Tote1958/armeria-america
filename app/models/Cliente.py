5ba390cc01b3d274b5f5de359809b4ebde003a01
Tote1958 <s.beltran@alumno.um.edu.ar>
2023-08-24 20:05:55 -0300

    Flask iniciado y clase Client terminada

--- app/models/Cliente.py
+++ app/models/Cliente.py
@@ -1,6 +1,55 @@
 from .. import db
 
-class Client(db.Model):
+class Client:
     __tablename__ = 'clients'
+    __name = ''
+    __dni = ''
+    __client_code = ''
+    __address = ''
+    __email = ''
 
-    __id = db.Column('id',)
+    @property
+    def client_code(self)->str:
+        return self.__client_code
+    
+    @client_code.setter
+    def client_code(self, code:str):
+        self.__client_code = code
+    
+    @property
+    def name(self)->str:
+        return self.__name
+    
+    @client_code.setter
+    def name(self, name:str):
+        self.__name = name
+    
+    @property
+    def dni(self)->str:
+        return self.__dni
+    
+    @client_code.setter
+    def dni(self, dni:str):
+        self.__dni = dni
+    
+    @property
+    def address(self)->str:
+        return self.__address
+    
+    @address.setter
+    def address(self, address:str):
+        self.__address = address
+
+    @property
+    def email(self)->str:
+        return self.__email
+    
+    @email.setter
+    def email(self, email:str):
+        self.__email = email
+    
+    def __repr__(self) -> str:
+        return f'Name: {self.__name},\nDNI: {self.__dni},\nCodigo de cliente: {self.__client_code},\nDireccion: {self.__address},\nEmail: {self.__email}'
+    
+    def __eq__(self, value:object) -> bool:
+        return self.dni == value.dni

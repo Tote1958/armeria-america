111b8176c4b43cdcaeb42dd400ab3730764a5ebc
Franco Roldan <fr.roldan@alumno.um.edu.ar>
2023-08-24 20:05:58 -0300

    #8 update Product.py

---
+++ app/models/Product.py
@@ -0,0 +1,62 @@
+class Product(db.Model):
+    __tablename__ = 'products'
+    __name = ''
+    __caliber = ''
+    __brand = ''
+    __description = ''
+    __type = ''
+    __serial_number = ''
+
+    @property # Es un decorador, hace un patron de diseño, le agrega funcionalidad al método en este caso, lo encapsula en un atributo, puede estar arriba de una clase o arriba de 
+    def name(self)->str: # Sería el get name
+        return self.__name
+    
+    @name.setter  # Sería el set name
+    def name(self, value:str):
+        self.__name = value
+
+    @property
+    def caliber(self)->str: # Sería el get caliber
+        return self.__caliber
+    
+    @caliber.setter  # Sería el set caliber
+    def caliber(self, value:str):
+        self.__caliber = value
+    
+    @property
+    def brand(self)->str: # Sería el get brand
+        return self.__brand
+    
+    @brand.setter  # Sería el set brand
+    def brand(self, value:str):
+        self.__brand = value
+
+    @property
+    def description(self)->str: # Sería el get description
+        return self.__description
+    
+    @description.setter  # Sería el set description
+    def description(self, value:str):
+        self.__description = value
+    
+    @property
+    def type(self)->str: # Sería el get type
+        return self.__type
+    
+    @type.setter  # Sería el set type
+    def type(self, value:str):
+        self.__type = value
+    
+    @property
+    def serial_number(self)->str: # Sería el get serial number
+        return self.__serial_number
+    
+    @serial_number.setter  # Sería el set serial number
+    def serial_number(self, value:str):
+        self.__serial_number = value
+
+    def __repr__(self) -> str:  # Da la info de cada objeto
+        return f'Product: \n Nombre: {self.name} \n Calibre: {self.caliber} \n Marca: {self.brand} \n Descripción: {self.description} \n Tipo: {self.type} \n Número de Serie: {self.serial_number}'    
+
+    def __eq__(self, value: object) -> bool: # Es para comparar si dos objetos están en la BD
+        return self.serial_number == value.serial_number

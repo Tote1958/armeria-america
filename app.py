07a561cf85f9133923b68290b4b0daf18f536259
Tote1958 <s.beltran@alumno.um.edu.ar>
2023-09-01 17:35:58 -0300

    #14 Flask run arreglado

--- app.py
+++ app.py
@@ -1,18 +1,4 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-from flask_migrate import Migrate
-from app.config.database import db, FULL_URL_DB
+from app import app
 
-app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = FULL_URL_DB
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)
-
-migrate = Migrate()
-migrate.init_app(app, db)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
--- app/config/database.py
+++ app/config/database.py
@@ -9,9 +9,9 @@ load_dotenv(os.path.join(basedir,'.env'))
 
 
 db = SQLAlchemy()
+USER_DB='postgres'
+PASS_DB='admin'
+URL_DB='localhost'
+NAME_DB='armeria-america'
 
-USER_DB = os.getenv('USER_DB')
-PASS_DB = os.getenv('PASS_DB')
-URL_DB = os.getenv('URL_DB')
-NAME_DB = os.getenv('NAME_DB')
 FULL_URL_DB = f'postgresql://{USER_DB}:{PASS_DB}@{URL_DB}/{NAME_DB}'

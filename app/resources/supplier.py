7a4775fddb491c0ef839baaf98ee699121716948
Dante <d.illesca@alumno.um.edu.ar>
2023-09-08 18:45:32 -0300

    supplier updated

--- app/resources/supplier.py
+++ app/resources/supplier.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, jsonify
+from app.services.supplier_service import SupplierService
 
 supplier = Blueprint('supplier', __name__)
 
 @supplier.route('/supplier/', methods=['GET'])
 def index():
+    service = SupplierService()
+    lista = SupplierService.find_all()
     resp = jsonify('OK PROVEEDORES')
     resp.status_code = 200
     return resp

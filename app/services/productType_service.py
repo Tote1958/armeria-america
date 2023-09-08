d062a0ca604df83dd521ff82630783cf368eefa5
Pablo Coello <pablofranciscocoello@gmail.com>
2023-09-08 18:42:24 -0300

    correcciones en productType_service

--- app/services/productType_service.py
+++ app/services/productType_service.py
@@ -1,7 +1,14 @@
 from app.models import ProductType
 
-class ProductType_service:
+class ProductTypeService:
     def __init__(self):
         pass
+    '''
+    return a list of all product types
+    '''
     def get_all(self) -> []:
+        list_product_type = []
+        productType1 = ProductType('Silenciador', 'SR', 'Silenciador monolitico')
+        productType2 = ProductType('Mira', 'MA', 'Mira termica')
+        productType3 = ProductType('Fusil de Asalto', 'FA', 'Fusil de asalto de 5.56')
         return self.__repo.find_all()

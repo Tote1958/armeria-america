eeba5478fc82685dc70013816ad401325269eb0f
Tote1958 <s.beltran@alumno.um.edu.ar>
2023-09-15 20:29:30 -0300

    #21 find_by_all para supplier creado

--- app/resources/client.py
+++ app/resources/client.py
@@ -9,7 +9,7 @@ client = Blueprint('client',__name__)
 @client.route('/client/', methods=['GET'])
 def index():
     service = ClientService()
-    lista = service.find_all()
+    lista = ClientService.find_all()
     resp = jsonify('OK CLIENTE')
     resp.status_code = 200
     return resp
--- app/resources/supplier.py
+++ app/resources/supplier.py
@@ -2,14 +2,24 @@ from flask import Blueprint, jsonify
 from app.services.supplier_service import SupplierService
 from ..mapping.supplier_schema import SupplierSchema
 
-ps = SupplierSchema(many=True) # es para que devuelva varios objetos, este se usa para find_all
+supplier_schema_many = SupplierSchema(many=True) # es para que devuelva varios objetos, este se usa para find_all
+supplier_schema = SupplierSchema()
 supplier = Blueprint('supplier', __name__)
 
 @supplier.route('/supplier/', methods=['GET'])
 def index():
     service = SupplierService()
     list = service.find_all()
-    result = ps.dump(list)
+    result = supplier_schema_many.dump(list)
+    resp = jsonify(result)
+    resp.status_code = 200
+    return resp
+
+@supplier.route('/supplier/id/<int:id>', methods=['GET'])
+def find_by_id(id):
+    service = SupplierService()
+    object = service.find_by_id(id)
+    result = supplier_schema.dump(object)
     resp = jsonify(result)
     resp.status_code = 200
     return resp

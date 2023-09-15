afdea9ecbb5bde4451a23bf80d13da51d20fc64c
Tote1958 <s.beltran@alumno.um.edu.ar>
2023-09-15 20:48:18 -0300

    #22 find_all para client creado

--- app/resources/client.py
+++ app/resources/client.py
@@ -2,14 +2,16 @@ from flask import Blueprint, jsonify
 from app.services.client_service import ClientService
 from ..mapping.client_schema import ClientSchema
 
-ps = ClientSchema(many=True)
-
-client = Blueprint('client',__name__)
+client_schema_many = ClientSchema(many=True) # es para que devuelva varios objetos, este se usa para find_all
+client_schema = ClientSchema()
+client = Blueprint('client', __name__)
 
 @client.route('/client/', methods=['GET'])
 def index():
     service = ClientService()
-    lista = ClientService.find_all()
-    resp = jsonify('OK CLIENTE')
+    list = service.find_all()
+    result = client_schema_many.dump(list)
+    resp = jsonify(result)
     resp.status_code = 200
     return resp
+

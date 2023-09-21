98dd6661c543978672e457462828893f6ab1eaab
Franco Roldan <fr.roldan@alumno.um.edu.ar>
2023-09-21 16:16:12 -0300

    #24 find_all product creado, función index

--- app/resources/product.py
+++ app/resources/product.py
@@ -2,13 +2,15 @@ from flask import Blueprint, jsonify
 from app.services.product_service import ProductService
 from ..mapping.product_schema import ProductSchema
 
-ps = ProductSchema(many=True) # many=True es para cuando tienen que recibir varios parametros
+product_schema_many = ProductSchema(many=True) # many=True es para cuando tienen que recibir varios parametros
+product_schema = ProductSchema()
 product = Blueprint('product', __name__)
 
 @product.route('/products/', methods=['GET'])
 def index():
     service = ProductService()
     list = service.find_all()
-    resp = jsonify('OK PRODUCTOS')
+    products = product_schema_many.dump(list)
+    resp = jsonify(products)
     resp.status_code = 200
     return resp

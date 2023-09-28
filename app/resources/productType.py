465df718b06efcca9a039ff11da52fc673ec4378
Pablo Coello <pablofranciscocoello@gmail.com>
2023-09-28 17:20:18 -0300

    Update productType

--- app/resources/productType.py
+++ app/resources/productType.py
@@ -12,12 +12,14 @@ def index():
     list = service.find_all()
     result = productType_schema_many.dump(list) # ps.dump(list) es para cuando se espera una lista de objetos y dump es para convertir el objeto en un diccionario
     resp = jsonify(result)
+    resp.status_code = 200
+    return resp
 
 @productType.route('/productType/id/<int:id>', methods=['GET'])
 def find_by_id(id):
     service = ProductTypeService()
     object = service.find_by_id(id)
-    productType = productType_schema_many.dump(object) # ps.dump(object) es para cuando se espera un solo objeto y dump es para convertir el objeto en un diccionario
+    productType = productType_schema.dump(object) # productType_schema.dump(object) es para cuando se espera un solo objeto y dump es para convertir el objeto en un diccionario
     resp = jsonify(productType)
     resp.status_code = 200
     return resp

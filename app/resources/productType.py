d079857c7b384d6e264c715aed59d817aa14efa8
Pablo Coello <pablofranciscocoello@gmail.com>
2023-09-28 16:56:26 -0300

    #25 creation of find-by-id for ProductType

--- app/resources/productType.py
+++ app/resources/productType.py
@@ -2,22 +2,22 @@ from flask import Blueprint, jsonify
 from app.services.productType_service import ProductTypeService
 from app.mapping.productType_schema import ProductTypeSchema
 
-ps = ProductTypeSchema(many=True) # many=True significa que se espera una lista de objetos
+productType_schema_many = ProductTypeSchema(many=True) # many=True significa que se espera una lista de objetos
+productType_schema = ProductTypeSchema()
 productType = Blueprint('productType', __name__)
 
 @productType.route('/productType/', methods=['GET'])
 def index():
     service = ProductTypeService()
-    lista = service.find_all()
-    resp = jsonify('OK desde productType')
-    resp.status_code = 200
-    return resp
+    list = service.find_all()
+    result = productType_schema_many.dump(list) # ps.dump(list) es para cuando se espera una lista de objetos y dump es para convertir el objeto en un diccionario
+    resp = jsonify(result)
 
 @productType.route('/productType/id/<int:id>', methods=['GET'])
 def find_by_id(id):
     service = ProductTypeService()
     object = service.find_by_id(id)
-    productType = ps.dump(object) # ps.dump(object) es para cuando se espera un solo objeto y dump es para convertir el objeto en un diccionario
+    productType = productType_schema_many.dump(object) # ps.dump(object) es para cuando se espera un solo objeto y dump es para convertir el objeto en un diccionario
     resp = jsonify(productType)
     resp.status_code = 200
     return resp

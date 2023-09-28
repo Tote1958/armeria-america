c0dcde774aaa1c487aa5acad609ee111c6e0fbc2
Pablo Coello <pablofranciscocoello@gmail.com>
2023-09-28 16:49:40 -0300

    -29 creation of find-id for prosuct

--- app/resources/productType.py
+++ app/resources/productType.py
@@ -12,3 +12,12 @@ def index():
     resp = jsonify('OK desde productType')
     resp.status_code = 200
     return resp
+
+@productType.route('/productType/id/<int:id>', methods=['GET'])
+def find_by_id(id):
+    service = ProductTypeService()
+    object = service.find_by_id(id)
+    productType = ps.dump(object) # ps.dump(object) es para cuando se espera un solo objeto y dump es para convertir el objeto en un diccionario
+    resp = jsonify(productType)
+    resp.status_code = 200
+    return resp

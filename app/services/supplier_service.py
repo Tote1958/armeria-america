15eb97d4238cf3ebdf882aa1733db5eec1d4b28e
Dantee0 <d.illesca@alumno.um.edu.ar>
2023-09-28 19:44:01 -0300

    Update y create añadido a supplier repositorio

--- app/services/supplier_service.py
+++ app/services/supplier_service.py
@@ -12,20 +12,11 @@ class SupplierService:
 
     def find_all(self) -> []: #es un parametro de salida
         list_supplier = []
-        
         name = 'dante'
         code = '1321233215'
         cuil = '203211526305'
         email = 'dante07082001@gmail.com'
         supplier1 = Supplier(name, cuil, email, code)
         list_supplier.append( supplier1 )
-        """db.session.add_all(list_supplier) 
-        db.session.commit()""" #Esto guarda ese usuario en la
         return list_supplier
     
-
-    """    def create(self, entity: Supplier) -> Supplier:
-        return self.__repo.create(entity)
-    
-    def update(self, entity: Supplier, id:int) -> Supplier:
-        return self.__repo.update(entity, id)"""

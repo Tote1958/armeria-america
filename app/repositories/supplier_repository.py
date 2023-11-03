1b00974a433bc1dceb9b0cfa852520c53c4d4000
Dantee0 <d.illesca@alumno.um.edu.ar>
2023-11-03 18:02:39 -0300

    Supplier updated

--- app/repositories/supplier_repository.py
+++ app/repositories/supplier_repository.py
@@ -15,17 +15,6 @@ class SupplierRepository(Read, Update, Create, Delete):
     def find_by_id(self, id: int) -> Supplier: #Read
         return db.session.query(self.__model).filter(self.__model.id == id).one()
     
-    def update(self, supplier: Supplier, id: int) -> Supplier: #Update
-        entity = self.find_by_id(id)
-        entity.name = supplier.name
-        entity.email = supplier.email
-        entity.code = supplier.code
-        entity.cuil = supplier.cuil
-        entity.id = supplier.id
-        db.session.add(entity)
-        db.session.commit()
-        return entity
-    
     def create(sef, supplier: Supplier) -> db.Model: #Create
         db.session.add(supplier)
         db.session.commit()

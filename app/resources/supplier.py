a1fef789b6a5126f7aa76f54055f30e5a2f62976
Dantee0 <d.illesca@alumno.um.edu.ar>
2023-11-16 19:01:50 -0300

    Supplier updated

--- app/resources/supplier.py
+++ app/resources/supplier.py
@@ -32,14 +32,14 @@ def create_supplier(): #anduvo
     return {"supplier": supplier_schema.dump(service.create(supplier))}, 200
 
 @supplier.route('/supplier/name/<string:name>', methods=['GET'])
-def find_by_name(name):
+def find_by_name(name): #anduvo
     service = SupplierService()
     object = service.find_by_name(name)
     result = supplier_schema.dump(object)
     return jsonify(result), 200
 
 @supplier.route('/supplier/email/<string:email>', methods=['GET'])
-def find_by_email(email):
+def find_by_email(email): #anduvo
     service = SupplierService()
     response = supplier_schema.dump(service.find_by_email(email))
     if response:
@@ -50,12 +50,12 @@ def find_by_email(email):
         return response_schema.dump((response_builder.build())), 400
 
 @supplier.route('/supplier/update/<int:id>', methods=['PUT'])
-def update(id):
+def update(id): 
     service = SupplierService()
     supplier = request.json
     return {"supplier":supplier_schema.dump(service.update(supplier, id))}, 200
 
 @supplier.route('/supplier/delete/<int:id>', methods=['DELETE'])
-def delete_supplier(id):
+def delete_supplier(id): #anduvo
     service = SupplierService()
     return {"delete supplier": supplier_schema.dump(service.delete(id))}, 200                

cd35d43da822232869a302a06ffcde8eaa6b6391
Dantee0 <d.illesca@alumno.um.edu.ar>
2023-11-17 16:01:11 -0300

    Supplier updated

--- app/repositories/supplier_repository.py
+++ app/repositories/supplier_repository.py
@@ -1,7 +1,7 @@
-from app.models import Supplier #En el repository va la comunicacion con la base de datos
+from app.models import Supplier 
 from app.config.database import db
 from .CRUD import Create, Read, Update, Delete
-# es la ultima capa que esta al final con la base de datos, la que rosa con la base de datos
+
 
 class SupplierRepository(Read, Update, Create, Delete):
 
@@ -9,23 +9,23 @@ class SupplierRepository(Read, Update, Create, Delete):
         self.__model = Supplier
     
     
-    def find_all(self): #Read
+    def find_all(self): 
         return db.session.query(self.__model).all()
 
     def find_by_id(self, id: int) -> Supplier: #Read
         return db.session.query(self.__model).filter(self.__model.id == id).one()
     
-    def create(sef, supplier: Supplier) -> db.Model: #Create
+    def create(self, supplier: Supplier) -> db.Model: 
         db.session.add(supplier)
         db.session.commit()
         return supplier
     
     def find_by_name(self, name: str) -> list:
-        list = db.session.query(self.__model).filter(self.__model.name.like(name)).all()
+        list = db.session.query(self.__model).filter(self.__model.name.like(name)).all() 
         return list
     
     def delete(self, id: int):
-        entity = self.find_by_id(id)
+        entity = self.find_by_id(id) 
         db.session.delete(entity)
         db.session.commit()
         return entity
@@ -33,7 +33,7 @@ class SupplierRepository(Read, Update, Create, Delete):
     def find_by_email(self, email: str) -> Supplier:
         return db.session.query(self.__model).filter(self.__model.email == email).first() 
     
-    def update(self,dto, id: int) -> Supplier: # data transfer object(dto) = es para pasar objectos
+    def update(self,dto, id: int) -> Supplier: # data transfer object(dto)
         entity = self.find_by_id(id)
         try:
             entity.name = dto["name"]
@@ -54,8 +54,3 @@ class SupplierRepository(Read, Update, Create, Delete):
         db.session.commit()
         return entity
     
-    def delete(self, id:int):
-        entity = self.find_by_id(id)
-        db.session.delete(entity)
-        db.session.commit()
-        return entity

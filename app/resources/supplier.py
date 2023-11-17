e10fbb7df0868a4ac5ec5caf1578e3e35b6901a5
Dantee0 <d.illesca@alumno.um.edu.ar>
2023-11-17 00:28:40 -0300

    Supplier updated

--- app/resources/supplier.py
+++ app/resources/supplier.py
@@ -4,6 +4,7 @@ from ..mapping.supplier_schema import SupplierSchema
 from ..models.response_message import ResponseBuilder
 from ..mapping.response_schema import ResponseSchema
 
+service = SupplierService()
 supplier_schema_many = SupplierSchema(many=True) # es para que devuelva varios objetos, este se usa para find_all
 supplier_schema = SupplierSchema()
 response_schema = ResponseSchema() 
@@ -12,7 +13,6 @@ supplier = Blueprint('supplier', __name__)
 
 @supplier.route('/supplier/', methods=['GET'])
 def index(): #anduvo
-    service = SupplierService()
     list = service.find_all()
     result = supplier_schema_many.dump(list)
     resp = jsonify(result)
@@ -21,19 +21,16 @@ def index(): #anduvo
 
 @supplier.route('/supplier/id/<int:id>', methods=['GET']) #en <int:id> pongo el id que quiero buscar
 def find_by_id(id): #anduvo
-    service = SupplierService()
     response_builder = ResponseBuilder("Usuario encontrado", 100, supplier_schema.dump(service.find_by_id(id)))
     return response_schema.dump((response_builder.build())), 200
 
 @supplier.route('/supplier/create', methods=['POST'])
 def create_supplier(): #anduvo
-    service = SupplierService()
     supplier = supplier_schema.load(request.json)
     return {"supplier": supplier_schema.dump(service.create(supplier))}, 200
 
 @supplier.route('/supplier/search/', methods=['GET'])
 def find_by_name(): #anduvo
-     service = SupplierService()
      name = request.args.get('name')
      response_builder = ResponseBuilder()
      response = supplier_schema_many.dump(service.find_by_name(name))
@@ -47,7 +44,6 @@ def find_by_name(): #anduvo
 
 @supplier.route('/supplier/email/<string:email>', methods=['GET'])
 def find_by_email(email): #anduvo
-    service = SupplierService()
     response = supplier_schema.dump(service.find_by_email(email))
     if response:
         response_builder = ResponseBuilder("Email encontrado",100, supplier_schema.dump(service.find_by_email(email)))
@@ -58,7 +54,6 @@ def find_by_email(email): #anduvo
 
 @supplier.route('/supplier/update/<int:id>', methods=['PUT'])
 def update(id): #anduvo
-    service = SupplierService()
     supplier = request.json
     return {"supplier":supplier_schema.dump(service.update(supplier, id))}, 200
 

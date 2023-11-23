dd47dcc8bc12473f64b16ac95c45fc49f98d6e73
Pablo Coello <pablofranciscocoello@gmail.com>
2023-11-23 09:54:27 -0300

    documentacion de productType

--- app/mapping/productType_schema.py
+++ app/mapping/productType_schema.py
@@ -4,6 +4,8 @@ from marshmallow import validate, fields, Schema, post_load
 #dump_only=True significa que no se puede escribir en el campo, solo leer
 #required=True significa que el campo es obligatorio
 #load_only=True significa que el campo solo se puede escribir, no leer
+
+"""Clase para mapear los datos del tipo de producto y adaptarlos a JSON"""
 class ProductTypeSchema(Schema):
     id = fields.Integer(dump_only=True)
     name = fields.String(required=True, validate=validate.Length(min=1, max=100))
--- app/repositories/productType_repository.py
+++ app/repositories/productType_repository.py
@@ -6,6 +6,7 @@ class ProductTypeRepository(Create, Read, Update, Delete):
     def __init__(self):
         self.__model = ProductType
 
+    """Recibe un JSON con los datos del tipo de producto y lo guarda en la base de datos"""
     #Create
     def create(self, dto: ProductType) -> db.Model:
         # entity = ProductType(name=dto['name'], code=dto['code'], description=dto['description'])
@@ -13,19 +14,24 @@ class ProductTypeRepository(Create, Read, Update, Delete):
         db.session.commit()
         return dto
 
+    """Hacemos una busqueda por id del tipo de producto"""
     #Read
     def find_by_id(self, id: str) -> ProductType:
         return db.session.query(self.__model).filter(self.__model.id == id).one()
 
+    """Hacemos una busqueda por nombre del tipo de producto"""
     def find_by_name(self, name: str) -> ProductType:
         return db.session.query(self.__model).filter(self.__model.name == name).one()
-    
+
+    """Hacemos una busqueda por codigo del tipo de producto"""    
     def find_by_code(self, code: str) -> ProductType:
         return db.session.query(self.__model).filter(self.__model.code == code).one()
-    
+
+    """Hacemos una busqueda y nos trae todos los tipos de productos"""    
     def find_all(self):
         return db.session.query(self.__model).all()
-    
+
+    """Recibe un JSON con los datos del tipo de producto y lo modifica en la base de datos""" 
     #Update
     def update(self, dto, id: int) -> ProductType:
         entity = self.find_by_id(id)
@@ -34,6 +40,7 @@ class ProductTypeRepository(Create, Read, Update, Delete):
         db.session.commit()
         return entity
     
+    """Borramos el tipo de producto por su id"""
     #Delete
     def delete(self, id: int):
         entity = self.find_by_id(id)
